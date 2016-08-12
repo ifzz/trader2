9294cd52669247ed0c363752dd2917c4ffc3b5c4
huilu <huilu@sysnew.com>
2016-08-12 11:35:48 +0800

    mduser client init

---
+++ src/test/trader_mduser_client_test.c
@@ -0,0 +1,91 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <event2/bufferevent.h>
+#include <event2/util.h>
+#include <event2/event.h>
+#include <event2/buffer.h>
+
+#include "trader_mduser_client.h"
+
+typedef struct trader_mduser_client_test_def trader_mduser_client_test;
+struct trader_mduser_client_test_def {
+
+  struct event_base *pBase;
+  struct event* pSigIntEvt;
+  struct event* pSigTermEvt;
+  
+  trader_mduser_client* pApi;
+
+};
+
+static void trader_mduser_client_test_connect_callback(void* user_data);
+static void trader_mduser_client_test_disconnect_callback(void* user_data);
+static void trader_mduser_client_test_recv_callback(void* user_data, void* data, int len);
+
+
+static void client_test_signal_cb(evutil_socket_t fd, short event, void *arg);
+
+void client_test_signal_cb(evutil_socket_t fd, short event, void *arg)
+{
+  trader_mduser_client_test* test = (trader_mduser_client_test*)arg;
+  struct timeval tv = {
+    2, 0
+  };
+  event_base_loopexit(test->pBase, &tv);
+}
+
+void trader_mduser_client_test_connect_callback(void* user_data)
+{
+  printf("connected!\n");
+}
+
+void trader_mduser_client_test_disconnect_callback(void* user_data)
+{
+  printf("disconnected!\n");
+  trader_mduser_client_test* test = (trader_mduser_client_test*)user_data;
+  test->pApi->method->xConnect(test->pApi);
+}
+
+void trader_mduser_client_test_recv_callback(void* user_data, void* data, int len)
+{
+  printf("recv[%d][%s]\n", len, (char*)data);
+}
+
+
+int main(int argc, char* argv[])
+{
+  trader_mduser_client_test trader_mduser_client_test_st;
+  trader_mduser_client_test* test = &trader_mduser_client_test_st;
+
+  test->pBase = event_base_new();
+
+  test->pSigIntEvt = evsignal_new(test->pBase, SIGINT, client_test_signal_cb, (void *)test);
+  test->pSigTermEvt = evsignal_new(test->pBase, SIGTERM, client_test_signal_cb, (void *)test);
+  
+  test->pApi = trader_mduser_client_new();
+
+  test->pApi->method->xInit(test->pApi, test->pBase, "0.0.0.0", 22345,
+    trader_mduser_client_test_connect_callback,
+    trader_mduser_client_test_disconnect_callback,
+    trader_mduser_client_test_recv_callback,
+    test
+    );
+
+  test->pApi->method->xConnect(test->pApi);
+
+  event_base_dispatch(test->pBase);
+
+  test->pApi->method->xExit(test->pApi);
+
+  event_free(test->pSigTermEvt);
+  event_free(test->pSigIntEvt);
+
+  trader_mduser_client_free(test->pApi);
+
+  return 0;
+}
+
+
